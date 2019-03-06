0c791b00627cfed166b5f9bd756b096bc7812311
Dzmitriy <Dzmitriy_Kakhanouski@epam.com>
2019-03-06 11:08:39 +0300

    add rebuild solution

---
+++ TestGtk/TestGtk/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gtk;
+using TestLib;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButton1Pressed(object sender, EventArgs e)
+    {
+        UserManager userManager = new UserManager(this.entry2.Text);
+
+        var dialog = new MessageDialog(this, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, userManager.GetGreeting());
+
+        dialog.Run();
+        dialog.Destroy();
+    }
+}
